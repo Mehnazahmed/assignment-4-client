d881bdd4bce253c0087b1a752ee2dc2bd8bf1b15
mehnaz <mehnazchowdhury613@gmail.com>
2024-07-08 23:12:37 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>assignment-4</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,18 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  endpoints: (builder) => ({
+    getPlantsCategories: builder.query({
+      query: () => ({
+        method: "GET",
+        url: "/categories",
+      }),
+    }),
+  }),
+});
+
+export const { useGetPlantsCategoriesQuery } = baseApi;
